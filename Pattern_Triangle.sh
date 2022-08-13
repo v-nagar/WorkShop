2b65037651a3bfa309cd1847561603c28026aa87
Vishal Nagar <110045911+v-nagar@users.noreply.github.com>
2022-08-13 17:08:03 +0530

    Update Pattern_Triangle.sh

--- Pattern_Triangle.sh
+++ Pattern_Triangle.sh
@@ -8,3 +8,43 @@ read -p " Enter second no : " y
 			done
 		echo " "
 	done
+	
+	echo "-----------------------  "
+
+
+ for (( var=1; var<=$x; var++ ))
+        do
+                for(( z=0; z<=$y; z++ ))
+                        do
+                                echo -n "*"
+                        done
+                echo " "
+        done
+ echo "-----------------------  "
+ for (( var=0; var<$x; var++ ))
+        do
+                for(( z=$y; z>var; z-- ))
+                        do
+                                echo -n "*"
+                        done
+                echo " "
+        done
+ echo "-----------------------  "
+
+ for (( var=0; var<$x; var++ ))
+        do
+                for(( z=0; z<=var; z++ ))
+                        do
+                                echo -n "*"
+                        done
+                echo " "
+        done
+for (( var=0; var<$x; var++ ))
+        do
+                for(( z=$y; z>var; z-- ))
+                        do
+                                echo -n "*"
+                        done
+                echo " "
+        done
+
